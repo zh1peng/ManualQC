b4b7f508d1f7f7c4ccf4fae84848e210a3f7c97a
懒麻蛇 <frueddy@gmail.com>
2017-10-24 15:48:42 +0100

    tiny fix

--- manualqc1.1/eegplugin_manualqc.m
+++ manualqc1.1/eegplugin_manualqc.m
@@ -1,4 +1,3 @@
 function eegplugin_manualqc( fig, try_strings, catch_strings)
-vers='manualqc1.1';
 toolsmenu = findobj(fig, 'tag', 'tools');
 uimenu( toolsmenu, 'label', 'ManualQC ', 'callback', 'manualqc;');
