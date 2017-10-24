d8a1b2b01f95bf8b252cf6c9e0c4c24ca0ea2ea3
懒麻蛇 <frueddy@gmail.com>
2017-10-24 17:04:26 +0100

    add info

--- manualqc1.1/eegplugin_manualqc.m
+++ manualqc1.1/eegplugin_manualqc.m
@@ -1,3 +1,40 @@
+% eegplugin_manualqc provides GUI for doing quality control (QC) 
+% manually on the EEG data. 
+% If you find any bugs,please contact me via github. 
+% Welcome to fork/star that on my github: https://github.com/zh1peng/ManualQC
+% Please mention cite this tool [repository] if you use it for your paper.
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%	
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% (C) 2017 Zhipeng Cao
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%v1.0 initial version
+%v1.1 mainly added in a new button which allows users to do ICs remeval first
+%        added to eegplugin
+%        fixed the save bug
+%        automatically calculate how many trials are marked as bad trials
+
+
 function eegplugin_manualqc( fig, try_strings, catch_strings)
 toolsmenu = findobj(fig, 'tag', 'tools');
 uimenu( toolsmenu, 'label', 'ManualQC ', 'callback', 'manualqc;');
--- manualqc1.1/manualqc.m
+++ manualqc1.1/manualqc.m
@@ -1,4 +1,36 @@
-%Author Info
+% manualqc provides GUI for doing quality control (QC)  manually on the EEG data. 
+% Author: Zhipeng Cao 
+% If you find any bugs,please contact me via github. 
+% Welcome to fork/star that on my github: https://github.com/zh1peng/ManualQC
+% Please mention cite this tool [repository] if you use it for your paper.
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%v1.0 initial version
+%v1.1 mainly added in a new button which allows users to do ICs remeval first
+%        added to eegplugin
+%        fixed the save bug
+%        automatically calculate how many trials are marked as bad trials
+
 function manualqc()
 line1='Using this GUI will clear all variables in the workspace and close all figures (including eeglab).\n';
 line2= 'If you have any unsaved works, press Cancel to go back.';
